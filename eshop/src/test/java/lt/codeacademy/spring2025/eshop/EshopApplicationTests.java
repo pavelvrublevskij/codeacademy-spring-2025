2ad9c6336b4a9cc5ef7c4eac1e833c52893941a3
Pavel Vrublevskij <pavel.vrublevskij@nortal.com>
2025-04-01 22:03:38 +0300

    2. [Spring MVC] add initial project with first controller and template engine (thymeleaf)

---
+++ eshop/src/test/java/lt/codeacademy/spring2025/eshop/EshopApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.spring2025.eshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
