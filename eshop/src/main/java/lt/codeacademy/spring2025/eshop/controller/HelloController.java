e7ff8b885ac9e3e88be33c0246200c67e251787d
Pavel Vrublevskij <pavel.vrublevskij@nortal.com>
2025-04-02 18:48:34 +0300

    2. [Spring MVC] log request query params using @RequestParam

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/controller/HelloController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/controller/HelloController.java
@@ -2,12 +2,16 @@ package lt.codeacademy.spring2025.eshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
 
 	@GetMapping("/hello")
-	public String sayHello() {
+	public String sayHello(@RequestParam String name, @RequestParam int age) {
+		System.out.println(name);
+		System.out.println(age);
 		return "hello.html";
 	}
+
 }
