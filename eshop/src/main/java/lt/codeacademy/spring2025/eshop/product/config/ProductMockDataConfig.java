98bb7068072cac72c6362d867de2dca08e5a9df9
Pavel Vrublevskij <pavel.vrublevskij@nortal.com>
2025-04-03 20:50:43 +0300

    3. [Spring MVC] use lombok external library log4j2 with configuration enabled option for debug on package level

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/config/ProductMockDataConfig.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/config/ProductMockDataConfig.java
@@ -4,11 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import com.github.javafaker.Faker;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lt.codeacademy.spring2025.eshop.core.domain.Product;
 import lt.codeacademy.spring2025.eshop.product.service.ProductService;
 
 @Configuration
 @RequiredArgsConstructor
+@Log4j2
 public class ProductMockDataConfig {
 
   private static final int MAX_COUNT = 10;
@@ -17,6 +19,7 @@ public class ProductMockDataConfig {
 
   @Bean
   public Void intiProducts() {
+    log.atDebug().log("=== Start initializing product mock data ===");
     var count = 0;
     final Faker faker = new Faker();
     while (MAX_COUNT >= count) {
@@ -28,6 +31,7 @@ public class ProductMockDataConfig {
       count++;
     }
 
+    log.atDebug().log("=== Finished Initializing product mock data ===");
     return null;
   }
 }
--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/service/ProductService.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/service/ProductService.java
@@ -3,16 +3,19 @@ package lt.codeacademy.spring2025.eshop.product.service;
 import java.util.List;
 import org.springframework.stereotype.Service;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import lt.codeacademy.spring2025.eshop.core.domain.Product;
 import lt.codeacademy.spring2025.eshop.product.repository.ProductRepository;
 
 @Service
 @RequiredArgsConstructor
+@Log4j2
 public class ProductService {
 
 	private final ProductRepository productRepository;
 
 	public void save(final Product product) {
+//    log.atInfo().log("=== Saving product ===");  // not recommended to use mostly in project because of performance issue
 		productRepository.save(product);
 	}
 
