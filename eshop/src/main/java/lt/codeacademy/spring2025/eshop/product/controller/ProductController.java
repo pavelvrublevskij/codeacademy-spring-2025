4439990ae9cfe7331d28dde873cc00d67ded6096
Pavel Vrublevskij <pavel.vrublevskij@nortal.com>
2025-04-03 18:38:33 +0300

    3. [Spring MVC] endpoint refactoring using RequestMapping on class level

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/controller/ProductController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/controller/ProductController.java
@@ -1,10 +1,10 @@
 package lt.codeacademy.spring2025.eshop.product.controller;
 
-import java.util.Collections;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import lombok.RequiredArgsConstructor;
 import lt.codeacademy.spring2025.eshop.product.dto.ProductDto;
 import lt.codeacademy.spring2025.eshop.product.mapper.ProductDtoMapper;
@@ -12,24 +12,25 @@ import lt.codeacademy.spring2025.eshop.product.service.ProductService;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/products")
 public class ProductController {
 
 	private final ProductService productService;
 	private final ProductDtoMapper productDtoMapper;
 
-	@GetMapping("/products")
+	@GetMapping
 	public String getProducts(Model model) {
 		model.addAttribute("productList", productService.getAllProducts().stream().map(productDtoMapper::toProductDto).toList());
 		return "product/products";
 	}
 
-	@GetMapping("/products/create")
+	@GetMapping("/create")
 	public String openProductForCreate(Model model) {
 		model.addAttribute("product", ProductDto.builder().build());
 		return "product/product";
 	}
 
-	@PostMapping("/products/create")
+	@PostMapping("/create")
 	public String createProduct(ProductDto product) {
 		productService.save(productDtoMapper.toProduct(product));
 		return "home";
