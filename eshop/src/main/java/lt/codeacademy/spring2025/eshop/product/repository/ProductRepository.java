75bb3513669df3902b16fafb16e04d54688d6a3f
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-08 21:00:22 +0300

    4. [Spring MVC] List -> Map in repository

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/repository/ProductRepository.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package lt.codeacademy.spring2025.eshop.product.repository;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
+import java.util.*;
+
 import org.springframework.stereotype.Repository;
 import lombok.RequiredArgsConstructor;
 import lt.codeacademy.spring2025.eshop.core.domain.Product;
@@ -16,33 +14,24 @@ public class ProductRepository {
 
 	private final ProductEntityMapper productEntityMapper;
 
-	private final List<ProductEntity> productEntities = new ArrayList<>();
+	private final Map<UUID, ProductEntity> productEntities = new HashMap<>();
 
 	public void save(final Product product) {
-    product.setId(UUID.randomUUID());
-		productEntities.add(productEntityMapper.toProductEntity(product));
+    final UUID productUUID = UUID.randomUUID();
+    product.setId(productUUID);
+    productEntities.put(productUUID, productEntityMapper.toProductEntity(product));
 	}
 
 	public List<Product> findAll() {
-		return productEntities.stream().map(productEntityMapper::toProduct).toList();
+    return productEntities.values().stream().map(productEntityMapper::toProduct).toList();
 	}
 
   public void update(final Product product) {
-    productEntities.stream()
-      .filter(productEntity -> productEntity.getId().equals(product.getId()))
-      .findFirst()
-      .ifPresent(productEntity -> {
-        productEntity.setName(product.getName());
-        productEntity.setPrice(product.getPrice());
-        productEntity.setDescription(product.getDescription());
-        productEntity.setAmountInStock(product.getAmount());
-      });
+    productEntities.put(product.getId(), productEntityMapper.toProductEntity(product));
   }
 
   public Optional<Product> findById(final UUID productId) {
-    return productEntities.stream()
-      .filter(productEntity -> productEntity.getId().equals(productId))
-      .map(productEntityMapper::toProduct)
-      .findFirst();
+    return Optional.ofNullable(productEntities.get(productId))
+      .map(productEntityMapper::toProduct);
   }
 }
