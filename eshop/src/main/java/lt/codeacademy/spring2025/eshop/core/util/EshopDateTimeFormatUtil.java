a68381c5165632998d594297a328ca046a8eeee9
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-17 19:11:18 +0300

    5. [Spring MVC] create static method in date time util class and reuse where needed

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/core/util/EshopDateTimeFormatUtil.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/core/util/EshopDateTimeFormatUtil.java
@@ -1,12 +1,19 @@
 package lt.codeacademy.spring2025.eshop.core.util;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
-public final class EshopFormatConstant {
+public final class EshopDateTimeFormatUtil {
 
   public static final String DATE_FORMAT = "yyyy-MM-dd";
   public static final String TIME_FORMAT = "HH:mm:ss";
   public static final String DATE_TIME_FORMAT = DATE_FORMAT + " " + TIME_FORMAT;
+
+  public static String getFormatterDateTime(LocalDateTime localDateTime) {
+    return DateTimeFormatter.ofPattern(DATE_TIME_FORMAT).format(localDateTime);
+  }
 }
--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
@@ -1,21 +1,20 @@
 package lt.codeacademy.spring2025.eshop.home.controller;
 
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import lt.codeacademy.spring2025.eshop.HttpEndpoint;
-import lt.codeacademy.spring2025.eshop.core.util.EshopFormatConstant;
+import lt.codeacademy.spring2025.eshop.core.util.EshopDateTimeFormatUtil;
 
 @Controller
 public class HomeController {
 
 	@GetMapping(HttpEndpoint.HOME)
 	public String openHomePage(Model model) {
-    final String formatterDateTime = DateTimeFormatter.ofPattern(EshopFormatConstant.DATE_TIME_FORMAT).format(LocalDateTime.now());
-    model.addAttribute("dateTime", formatterDateTime);
+    model.addAttribute("dateTime",
+      EshopDateTimeFormatUtil.getFormatterDateTime(LocalDateTime.now()));
 		return "home";
 	}
 
