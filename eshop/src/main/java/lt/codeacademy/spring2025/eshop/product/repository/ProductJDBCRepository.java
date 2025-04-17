b5f2dc14752fc71671d96af65d8e48cdc0e09389
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-17 21:30:25 +0300

    5. [Spring JPA] working with db data from repository layer. Use @Primary to let spring to know that this bean is primary injected

---
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/repository/ProductJDBCRepository.java
@@ -0,0 +1,38 @@
+package lt.codeacademy.spring2025.eshop.product.repository;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Repository;
+import lt.codeacademy.spring2025.eshop.core.domain.Product;
+
+@Primary
+@Repository
+public class ProductJDBCRepository implements ProductRepository {
+  @Override
+  public void save(Product product) {
+
+  }
+
+  @Override
+  public List<Product> findAll() {
+    return List.of();
+  }
+
+  @Override
+  public void update(Product product) {
+
+  }
+
+  @Override
+  public Optional<Product> findById(UUID productId) {
+    return Optional.empty();
+  }
+
+  @Override
+  public void deleteProductByUUID(UUID productId) {
+
+  }
+}
