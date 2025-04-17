99b3cd3a9a7314d205633153e0768e3126428101
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-17 18:56:26 +0300

    5. [Spring MVC] added date time with formatter into home page via model using attribute

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
@@ -1,6 +1,10 @@
 package lt.codeacademy.spring2025.eshop.home.controller;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import lt.codeacademy.spring2025.eshop.HttpEndpoint;
 
@@ -8,7 +12,9 @@ import lt.codeacademy.spring2025.eshop.HttpEndpoint;
 public class HomeController {
 
 	@GetMapping(HttpEndpoint.HOME)
-	public String openHomePage() {
+	public String openHomePage(Model model) {
+    final String formatterDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss").format(LocalDateTime.now());
+    model.addAttribute("dateTime", formatterDateTime);
 		return "home";
 	}
 
