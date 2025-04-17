42bd7d74d36696e7fdbe2747225399983a566c39
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-17 22:00:24 +0300

    5. [Spring JPA] working with db data from repository layer. Get all products, delete product by UUID implementation (JdbcTemplate)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/repository/ProductJDBCRepository.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/repository/ProductJDBCRepository.java
@@ -4,11 +4,17 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import lombok.RequiredArgsConstructor;
 import lt.codeacademy.spring2025.eshop.core.domain.Product;
 
 @Repository
+@RequiredArgsConstructor
 public class ProductJDBCRepository implements ProductRepository {
+
+  private final JdbcTemplate jdbcTemplate;
+
   @Override
   public void save(Product product) {
 
@@ -16,7 +22,14 @@ public class ProductJDBCRepository implements ProductRepository {
 
   @Override
   public List<Product> findAll() {
-    return List.of();
+    return jdbcTemplate.query("SELECT * FROM PRODUCT",
+            (resultSet, rowNum) -> Product.builder()
+                    .id(UUID.fromString(resultSet.getString("PRODUCT_ID")))
+                    .name(resultSet.getString("NAME"))
+                    .price(resultSet.getDouble("PRICE"))
+                    .amount(resultSet.getInt("QUANTITY_IN_STOCK"))
+                    .description(resultSet.getString("DESCRIPTION"))
+                    .build());
   }
 
   @Override
@@ -31,6 +44,6 @@ public class ProductJDBCRepository implements ProductRepository {
 
   @Override
   public void deleteProductByUUID(UUID productId) {
-
+    jdbcTemplate.execute(String.format("DELETE FROM PRODUCT WHERE PRODUCT_ID = '%s'", productId.toString()));
   }
 }
