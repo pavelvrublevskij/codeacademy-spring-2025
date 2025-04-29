0557a998b89040d509ce3e2a8a7346c7d62becaa
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-04-29 19:15:49 +0300

    9. [Spring MVC, i18n] show not translated and NPE message logic in fronted side. Catch exception as error and log into console (MessageSource, @Log4j2)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/helper/MessageService.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/helper/MessageService.java
@@ -1,18 +1,36 @@
 package lt.codeacademy.spring2025.eshop.helper;
 
 import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Component;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 
 @Component
 @RequiredArgsConstructor
+@Log4j2
 public class MessageService {
 
   private final MessageSource messageSource;
 
+  /**
+   * Used for message translation by provided key
+   * @param key from messages translation files
+   * @return Translated message by selected locale. Return ?text? if no translated key provided
+   */
   public String getTranslatedMessage(String key) {
-    return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+    try {
+      return messageSource.getMessage(key, null, LocaleContextHolder.getLocale());
+    } catch (NoSuchMessageException e) {
+      if (key != null && !key.equals("")) {
+        log.atError().log("Translation key not found: {}", key);
+
+        return String.format("?%s?", key);
+      }
+    }
+
+    return "";
   }
 
 }
