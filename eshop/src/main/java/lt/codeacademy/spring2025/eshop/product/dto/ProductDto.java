6a946fb9d8978058d2516b7ee4f55ccc3442dbd5
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-07 21:33:01 +0300

    12. [Spring Validation] add additional validations (Jakarta, javax-validation, JSR-380)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/dto/ProductDto.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/product/dto/ProductDto.java
@@ -2,8 +2,11 @@ package lt.codeacademy.spring2025.eshop.product.dto;
 
 import java.util.UUID;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -15,10 +18,15 @@ import lombok.ToString;
 @Builder
 public class ProductDto {
   private UUID id;
+  @NotBlank(message = "{productdto.name.notblank}")
 	private String name;
+  @Positive(message = "{productdto.price.positive}")
 	private double price;
-  @Positive(message = "{amount.positive}")
+  @Positive(message = "{productdto.amount.positive}")
   @NotNull
+  @Max(value = 10000)
 	private int amount;
+  @NotBlank(message = "{productdto.description.notblank}")
+  @Size(min = 10, message = "{productdto.description.min}")
   private String description;
 }
