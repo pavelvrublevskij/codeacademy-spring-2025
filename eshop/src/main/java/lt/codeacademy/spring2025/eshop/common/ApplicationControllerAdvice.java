bac0e1a77e4a81971156e65d05c2e8ede81c6fc6
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-08 18:37:23 +0300

    13. [Spring MVC] add global attribute (ControllerAdvice, ModelAttribute)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/common/ApplicationControllerAdvice.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/common/ApplicationControllerAdvice.java
@@ -1,12 +1,16 @@
 package lt.codeacademy.spring2025.eshop.common;
 
+import java.time.LocalDateTime;
+
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import lt.codeacademy.spring2025.eshop.common.exception.EshopNotFoundException;
+import lt.codeacademy.spring2025.eshop.core.util.EshopDateTimeFormatUtil;
 
 @ControllerAdvice
 public class ApplicationControllerAdvice {
@@ -20,5 +24,10 @@ public class ApplicationControllerAdvice {
   @InitBinder
   public void initBinder(WebDataBinder webDataBinder) {
     webDataBinder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+  }
+
+  @ModelAttribute("modelDateAttr")
+  public String now() {
+    return EshopDateTimeFormatUtil.getFormatterDateTime(LocalDateTime.now());
   }
 }
--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
@@ -1,20 +1,14 @@
 package lt.codeacademy.spring2025.eshop.home.controller;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import lt.codeacademy.spring2025.eshop.HttpEndpoint;
-import lt.codeacademy.spring2025.eshop.core.util.EshopDateTimeFormatUtil;
 
 @Controller
 public class HomeController {
 
 	@GetMapping(HttpEndpoint.HOME)
-	public String openHomePage(Model model) {
-    model.addAttribute("dateTime",
-      EshopDateTimeFormatUtil.getFormatterDateTime(LocalDateTime.now()));
+	public String openHomePage() {
 		return "home";
 	}
 
