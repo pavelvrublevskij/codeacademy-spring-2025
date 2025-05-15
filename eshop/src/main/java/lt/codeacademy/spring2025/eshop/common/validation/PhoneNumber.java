102e1df76900a2b831d703c95e93e1dd76df28c8
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-15 21:32:21 +0300

    16. [Spring Custom Validation] add simple phoneNumber validation annotation (ConstraintValidator, Optional, @Retention, @Target, @Constraint, @interface)

---
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/common/validation/PhoneNumber.java
@@ -0,0 +1,31 @@
+package lt.codeacademy.spring2025.eshop.common.validation;
+
+import java.lang.annotation.*;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotBlank;
+import lt.codeacademy.spring2025.eshop.common.validation.impl.PhoneNumberValidator;
+
+import static java.lang.annotation.ElementType.*;
+import static java.lang.annotation.ElementType.TYPE_USE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Constraint(validatedBy = { PhoneNumberValidator.class })
+@NotBlank
+public @interface PhoneNumber {
+  String message() default "Phone number length must be 12 characters";
+
+  Class<?>[] groups() default { };
+
+  Class<? extends Payload>[] payload() default { };
+
+  @Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
+  @Retention(RUNTIME)
+  @Documented
+  public @interface List {
+    NotBlank[] value();
+  }
+}
