977f11ca608e0f56a736763d4669194d04208a4f
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-20 22:01:51 +0300

    17. [Spring MVC, Session] make cart template with hardcoded values from controller product dto data (temp data) (i18n, @Builder, record, List, <table>, bootstrap, th:text)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/cart/controller/CartController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/cart/controller/CartController.java
@@ -1,10 +1,15 @@
 package lt.codeacademy.spring2025.eshop.cart.controller;
 
+import java.math.BigDecimal;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.Builder;
 
 import static lt.codeacademy.spring2025.eshop.cart.controller.CartController.CART_SESSION;
 
@@ -16,12 +21,23 @@ public class CartController {
   protected static final String CART_SESSION = "cartSession";
 
   @ModelAttribute(CART_SESSION)
-  public String createDefaultCartSession() {
-    return "Labas";
+  public List<CartProductDto> createDefaultCartSession() {
+    return List.of(CartProductDto.builder()
+      .name("Testas")
+      .unitPrice(BigDecimal.TEN)
+      .amount(1)
+      .build());
   }
 
   @GetMapping
   public String openCart() {
     return "cart/cart";
+  }
+
+  @Builder
+  public record CartProductDto(
+    String name,
+    BigDecimal unitPrice,
+    int amount) {
   }
 }
