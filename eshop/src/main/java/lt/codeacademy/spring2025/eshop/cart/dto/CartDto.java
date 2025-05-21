42fd23e3373223d40e2ef76ffe3f9355b055d7e1
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-21 21:53:36 +0300

    18. [Spring MVC, Session] calculate cart total price and total quantity

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/cart/dto/CartDto.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/cart/dto/CartDto.java
@@ -1,5 +1,6 @@
 package lt.codeacademy.spring2025.eshop.cart.dto;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -15,5 +16,17 @@ public class CartDto {
 
   public void addItem(final CartProductDto cartProductDto) {
     cartItems.add(cartProductDto);
+  }
+
+  public int cartTotalQuantity() {
+    return cartItems.stream()
+      .mapToInt(CartProductDto::quantity)
+      .sum();
+  }
+
+  public BigDecimal cartTotalPrice() {
+    return cartItems.stream()
+      .map(CartProductDto::totalPrice)
+      .reduce(BigDecimal.ZERO, BigDecimal::add);
   }
 }
