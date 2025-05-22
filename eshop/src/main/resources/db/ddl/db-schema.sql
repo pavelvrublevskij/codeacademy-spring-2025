2de4eef1d111d47bd0264d8cce1d4a378130e5e1
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-22 21:21:39 +0300

    19. [Spring MVC, Session] Fix H2 and MySql missed DDL's

--- eshop/src/main/resources/db/ddl/db-schema.sql
+++ eshop/src/main/resources/db/ddl/db-schema.sql
@@ -7,3 +7,23 @@ CREATE TABLE product
     price DECIMAL(20, 2) NOT NULL,
     description VARCHAR(500) DEFAULT NULL
 );
+
+CREATE TABLE product_category
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product_product_categories
+(
+    product_id          BIGINT NOT NULL,
+    product_category_id INT    NOT NULL
+);
+
+ALTER TABLE product_product_categories
+    ADD CONSTRAINT product_product_categories_category_id
+        FOREIGN KEY (product_category_id) REFERENCES product_category (id);
+
+ALTER TABLE product_product_categories
+    ADD CONSTRAINT product_product_categories_product_id
+        FOREIGN KEY (product_id) REFERENCES product (id);
--- eshop/src/main/resources/db/mysql/ddl/db-schema.sql
+++ eshop/src/main/resources/db/mysql/ddl/db-schema.sql
@@ -1,11 +1,33 @@
+DROP TABLE IF EXISTS product_product_categories;
+DROP TABLE IF EXISTS product_category;
 DROP TABLE IF EXISTS product;
 
 CREATE TABLE product
 (
     id                INT AUTO_INCREMENT PRIMARY KEY,
-    product_id        VARCHAR(36)            NOT NULL,
+    product_id        binary(36)            NOT NULL,
     name              VARCHAR(40)            NOT NULL,
     quantity_in_stock INT          DEFAULT 0 NOT NULL,
     price             DECIMAL(20, 2)         NOT NULL,
     description       VARCHAR(500) DEFAULT NULL
 );
+
+CREATE TABLE product_category
+(
+    id   INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE product_product_categories
+(
+    product_id          INT NOT NULL,
+    product_category_id INT NOT NULL
+);
+
+ALTER TABLE product_product_categories
+    ADD CONSTRAINT product_product_categories_category_id
+        FOREIGN KEY (product_category_id) REFERENCES product_category (id);
+
+ALTER TABLE product_product_categories
+    ADD CONSTRAINT product_product_categories_product_id
+        FOREIGN KEY (product_id) REFERENCES product (id);
