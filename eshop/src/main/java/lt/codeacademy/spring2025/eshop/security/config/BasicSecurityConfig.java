aee5ed4d6389b91cbd50bdac58aa433d68e3ef70
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-27 21:54:10 +0300

    20. [Spring Security Basic] make h2 data table accessible using WebSecurityCustomizer (ignore path for csrf, eshop_h2, @Bean)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/security/config/BasicSecurityConfig.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/security/config/BasicSecurityConfig.java
@@ -1,9 +1,10 @@
 package lt.codeacademy.spring2025.eshop.security.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,14 +28,14 @@ public class BasicSecurityConfig {
           .usernameParameter("loginEmail")
           .passwordParameter("loginPassword")
       )
-      .csrf(csrfConfigurer ->
-        csrfConfigurer.ignoringRequestMatchers(
-          "/eshop_h2/**"
-        )
-      )
-      .headers(headersConfigurer ->
-        headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin)
-      )
       .build();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring()
+      .requestMatchers(
+        PathRequest.toH2Console()
+      );
   }
 }
