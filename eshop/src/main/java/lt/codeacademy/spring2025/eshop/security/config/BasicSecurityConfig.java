74144183268bec4fa2ec8adfbd4b4ac3875d7bdc
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-05-27 20:35:42 +0300

    20. [Spring Security Basic] implement default spring login chain configuration using fluentApi (SecurityFilterChain, @Bean, @Configuration)

---
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/security/config/BasicSecurityConfig.java
@@ -0,0 +1,22 @@
+package lt.codeacademy.spring2025.eshop.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class BasicSecurityConfig {
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+      .authorizeHttpRequests(authorizeRequests ->
+        authorizeRequests.requestMatchers("/login/**").permitAll()
+          .anyRequest()
+          .authenticated())
+      .formLogin(formLogin ->
+        formLogin.permitAll())
+      .build();
+  }
+}
