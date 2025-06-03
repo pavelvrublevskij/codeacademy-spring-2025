a9012d9da0d5515b07cd80ec887ec131694c0531
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-06-03 21:45:35 +0300

    23. [Profiles] add additional config property to expose information on page (@PropertySource, @Value, thymeleaf)

--- eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
+++ eshop/src/main/java/lt/codeacademy/spring2025/eshop/home/controller/HomeController.java
@@ -1,14 +1,31 @@
 package lt.codeacademy.spring2025.eshop.home.controller;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import lt.codeacademy.spring2025.eshop.HttpEndpoint;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@PropertySource("classpath:eshop-company.properties")
 public class HomeController {
 
+  @Value("${company.name}")
+  private String companyName;
+  @Value("${company.address}")
+  private String companyAddress;
+  @Value("${company.color}")
+  private String companyColor;
+  @Value("${developer.name}")
+  private String developerName;
+
 	@GetMapping(HttpEndpoint.HOME)
-	public String openHomePage() {
+	public String openHomePage(Model model) {
+    model.addAttribute("companyName", companyName);
+    model.addAttribute("companyAddress", companyAddress);
+    model.addAttribute("developerName", developerName);
+    model.addAttribute("companyColor", companyColor);
 		return "home";
 	}
 
