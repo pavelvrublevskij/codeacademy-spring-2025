e84d807c6df4b4bd2d0e5d6f965d1239437e591d
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-15 19:59:31 +0300

    31. [React] move routes to a different container (refactoring)

--- eshop-ui/src/containers/App.tsx
+++ eshop-ui/src/containers/App.tsx
@@ -1,28 +1,15 @@
 import Header from './Header';
 import Footer from './Footer';
 import './App.css';
-import HomePage from './Pages/HomePage/HomePage';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import NewProductPage from './Pages/NewProductPage';
-import ProductsPage from './Pages/ProductsPage';
+import { BrowserRouter } from 'react-router-dom';
+import Pages from './Pages/Page';
 
 function App() {
     return (
         <BrowserRouter>
             <div className="mainApp">
                 <Header />
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    <Route path="/products" element={<ProductsPage />} />
-                    <Route
-                        path="/products/create"
-                        element={<NewProductPage />}
-                    />
-                    <Route
-                        path="/products/:productId"
-                        element={<ProductsPage />}
-                    />
-                </Routes>
+                <Pages />
                 <Footer />
             </div>
         </BrowserRouter>
