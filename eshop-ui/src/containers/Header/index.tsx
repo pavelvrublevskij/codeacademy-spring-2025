9bd32b81925d5698d41b2c731f6d8cb61f4304b8
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-15 18:54:09 +0300

    31. [React] use react-router-dom for SPA (single page application)

--- eshop-ui/src/containers/Header/index.tsx
+++ eshop-ui/src/containers/Header/index.tsx
@@ -4,11 +4,12 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import {NavLink} from "react-router-dom";
 
 const HeaderContainer = () =>
     <Navbar expand="lg" className="bg-body-tertiary" bg="dark" data-bs-theme="dark">
         <Container fluid>
-            <Navbar.Brand href="/">E-Shop</Navbar.Brand>
+            <Navbar.Brand to="/" as={NavLink}>E-Shop</Navbar.Brand>
             <Navbar.Toggle aria-controls="navbarScroll" />
             <Navbar.Collapse id="navbarScroll">
                 <Nav
@@ -16,9 +17,9 @@ const HeaderContainer = () =>
                     style={{ maxHeight: '100px' }}
                     navbarScroll
                 >
-                    <Nav.Link href="/">Home</Nav.Link>
-                    <Nav.Link href="/products">Products</Nav.Link>
-                    <Nav.Link href="/products/create">Create a Product</Nav.Link>
+                    <Nav.Link to="/" as={NavLink}>Home</Nav.Link>
+                    <Nav.Link to="/products" as={NavLink}>Products</Nav.Link>
+                    <Nav.Link to="/products/create" as={NavLink}>Create a Product</Nav.Link>
                     <NavDropdown title="Languages" id="navbarScrollingDropdown">
                         <NavDropdown.Item href="#action3">LT</NavDropdown.Item>
                         <NavDropdown.Item href="#action4">EN</NavDropdown.Item>
