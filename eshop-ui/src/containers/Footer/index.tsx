31056c2fd97ea786e76c7367bca5e0bd9e915d48
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-15 18:24:36 +0300

    31. [React] make footer align text centered (bootstrap selector text-center)

--- eshop-ui/src/containers/Footer/index.tsx
+++ eshop-ui/src/containers/Footer/index.tsx
@@ -2,7 +2,7 @@ import Container from "react-bootstrap/Container";
 
 const FooterContainer = () =>
     <footer className='fixed-bottom'>
-        <Container fluid={'sm'}>
+        <Container className={'text-center'}>
             <span className="text-muted">CodeAcademy. React Bootstrap. All Rights reserved </span>
         </Container>
     </footer>
