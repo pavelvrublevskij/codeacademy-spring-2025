37050f420e8411ca4f77ef85f42ad34a7abecba8
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-16 21:46:02 +0300

    32. [RESTFUL API] make file downloadable (Header Content Disposition)

--- eshop/api/src/main/java/lt/codeacademy/spring2025/eshop/api/controller/FilesRestController.java
+++ eshop/api/src/main/java/lt/codeacademy/spring2025/eshop/api/controller/FilesRestController.java
@@ -3,9 +3,13 @@ package lt.codeacademy.spring2025.eshop.api.controller;
 import java.io.IOException;
 
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.http.MediaType;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import lombok.RequiredArgsConstructor;
 import lt.codeacademy.spring2025.eshop.file.FileService;
@@ -25,8 +29,12 @@ public class FilesRestController {
 
   @GetMapping("/download")
   public ResponseEntity<ByteArrayResource> downloadFile(@RequestParam String fileName) {
+    final HttpHeaders headers = new HttpHeaders();
+    headers.setContentDispositionFormData("attachment", fileName);
+    headers.setContentType(fileService.getFileMediaTypeByFileName(fileName));
+
     return ResponseEntity.ok()
-      .contentType(fileService.getFileMediaTypeByFileName(fileName))
+      .headers(headers)
       .body(fileService.download(fileName));
   }
 }
