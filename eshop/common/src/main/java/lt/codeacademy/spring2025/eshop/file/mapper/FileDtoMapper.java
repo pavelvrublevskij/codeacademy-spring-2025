e7de05c434bb0666639424df962bd898d3dd94b3
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-17 21:49:23 +0300

    33. [Spring MVC] add table to represent stored files in database for possibility to download (thymeleaf, mappers, dto, domain)

---
+++ eshop/common/src/main/java/lt/codeacademy/spring2025/eshop/file/mapper/FileDtoMapper.java
@@ -0,0 +1,28 @@
+package lt.codeacademy.spring2025.eshop.file.mapper;
+
+import org.springframework.stereotype.Component;
+import lt.codeacademy.spring2025.eshop.common.mapper.DomainDtoMapper;
+import lt.codeacademy.spring2025.eshop.core.domain.file.File;
+import lt.codeacademy.spring2025.eshop.file.dto.FileDto;
+
+@Component
+public class FileDtoMapper implements DomainDtoMapper<File, FileDto> {
+
+  @Override
+  public FileDto toDto(final File domain) {
+    if (domain == null) {
+      return null;
+    }
+
+    return FileDto.builder()
+        .fileName(domain.getName())
+        .size(domain.getSize())
+        .build();
+  }
+
+  @Override
+  public File toDomain(final FileDto fileDto) {
+    // TODO: implement this method when needed
+    return null;
+  }
+}
