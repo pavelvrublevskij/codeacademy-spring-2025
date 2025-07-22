366b7be1af006f468c74fed9bfe045b7cd4bda25
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-22 19:54:14 +0300

    34. [Restful API, Spring Security Basic] add hardcoded security user and password into configuration using createDelegatingPasswordEncoder() method (PasswordEncoderFactories, PasswordEncoder)

--- eshop/security/security-rest-basic/src/main/java/lt/codeacademy/security/config/SecurityRestBasicConfig.java
+++ eshop/security/security-rest-basic/src/main/java/lt/codeacademy/security/config/SecurityRestBasicConfig.java
@@ -1,10 +1,18 @@
 package lt.codeacademy.security.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true, securedEnabled = true, jsr250Enabled = true)
 public class SecurityRestBasicConfig {
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+  }
 
 }
