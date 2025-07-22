e358d1a76b5d2e11c4222dec823814e2ed699df6
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-22 20:45:43 +0300

    34. [Restful API, Spring Security Basic] use standard way for rest security configuration using stateless session policy, off csrf and turn off standard spring security basic form implementation

--- eshop/security/security-rest-basic/src/main/java/lt/codeacademy/security/config/SecurityRestBasicConfig.java
+++ eshop/security/security-rest-basic/src/main/java/lt/codeacademy/security/config/SecurityRestBasicConfig.java
@@ -4,11 +4,15 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 
@@ -20,6 +24,24 @@ import lombok.extern.log4j.Log4j2;
 public class SecurityRestBasicConfig {
 
   private final ApplicationUsersPropertyConfig applicationUsersPropertyConfig;
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    return http
+      // Disable CSRF protection for REST APIs.
+      .csrf(AbstractHttpConfigurer::disable)
+
+      // turn on http basic authentication in inMemoryUserDetailsService.
+      .httpBasic(httpConfigurer -> httpConfigurer.init(http))
+
+      // ses session management to stateless, as REST APIs should not maintain session state.
+      .sessionManagement(sessionManagementConfigurer ->
+        sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+
+      // set authorization access to whole request.
+      .authorizeHttpRequests(authorizeRequest -> authorizeRequest.anyRequest().authenticated())
+      .build();
+  }
 
   @Bean
   public PasswordEncoder passwordEncoder() {
