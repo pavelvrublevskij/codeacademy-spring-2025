5d3848c530333fe7440156f4adec7b493ef8b256
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-23 21:10:00 +0300

    35. [Restful API, Spring Security JWT] create JWT token using login with securioty key and validity in spring configuration (@Value, application.yaml, auth0-jwt, HMAC512 Algorithm)

--- eshop/security/security-rest-jwt/src/main/java/lt/codeacademy/security/provider/JwtProvider.java
+++ eshop/security/security-rest-jwt/src/main/java/lt/codeacademy/security/provider/JwtProvider.java
@@ -1,14 +1,49 @@
 package lt.codeacademy.security.provider;
 
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lt.codeacademy.security.core.dto.UserRoleDto;
 
 @Component
 public class JwtProvider {
 
+  private static final Map<String, Object> HEADER_MAP;
+
+  @Value("#{${spring.security.jwt.validity-time-in-minutes} * 60 * 1000}")
+  private long tokenValidityInMinutes;
+
+  @Value("${spring.security.jwt.secret}")
+  private byte[] secretKey;
+
+  static {
+    HEADER_MAP = new HashMap<>();
+    HEADER_MAP.put("typ", "JWT");
+  }
+
   public String createToken(final Authentication authentication) {
     final UserRoleDto userRoleDto = (UserRoleDto) authentication.getPrincipal();
-    return "cia mano tokenas";
+    final Date now = new Date();
+
+    final String jwt = JWT.create()
+      .withHeader(HEADER_MAP)
+      .withIssuer("eshop-api")
+      .withAudience("eshop-api")
+      .withSubject(userRoleDto.getUsername())
+      .withClaim("roles", userRoleDto.getAuthorities().stream()
+        .map(GrantedAuthority::getAuthority)
+        .toList())
+      .withIssuedAt(now)
+      .withExpiresAt(new Date(now.getTime() + tokenValidityInMinutes))
+      .sign(Algorithm.HMAC512(secretKey));
+
+    return jwt;
   }
 }
