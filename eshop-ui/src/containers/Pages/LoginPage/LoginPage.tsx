c64af7691d9665aba9d0cbb60d1c743c39009420
Pavel Vrublevskij <pavel.vrublevskij@gmail.com>
2025-07-30 19:01:41 +0300

    37. [React] use formik isSubmiting props (Spinner)

--- eshop-ui/src/containers/Pages/LoginPage/LoginPage.tsx
+++ eshop-ui/src/containers/Pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,7 @@
 import { Field, Form, Formik, FormikProps } from 'formik';
 import {
     Button,
-    Container,
+    Container, Spinner,
 } from 'react-bootstrap';
 import FormikFieldInputGroup from '../../../components/Formik/FormikFieldInputGroup/FormikFieldInputGroup';
 
@@ -53,8 +53,23 @@ const LoginPage = () => {
                             <Form>
                                 <Field name="email" component={FormikFieldInputGroup} labelText="Email:" type="text" />
                                 <Field name="password" component={FormikFieldInputGroup} labelText="Password:" type="password" />
-                                <div>
-                                    <Button type='submit'>Submit</Button>
+                                <div className="text-center">
+                                    {props.isSubmitting
+                                        ? <Button variant='primary' disabled>
+                                            <Spinner
+                                                as='span'
+                                                animation='grow'
+                                                size='sm'
+                                                role='status'
+                                                aria-hidden='true'
+                                            />
+                                            Processing...
+                                        </Button>
+                                        :  <Button type='submit'
+                                                   variant='primary'>
+                                            Submit
+                                        </Button>
+                                    }
                                 </div>
                             </Form>
                         </Container>
